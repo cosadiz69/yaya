29940868cf0253ea467302396cc29ceca65673fd
EX69 <cosadiz69@gmail.com>
2022-06-12 07:33:56 +0800

    Update update.py

--- update.py
+++ update.py
@@ -37,7 +37,7 @@ try:
     if len(UPSTREAM_REPO) == 0:
        raise TypeError
 except:
-    UPSTREAM_REPO = "https://github.com/cosadiz69/yaya"
+    UPSTREAM_REPO = "https://www.github.com/anasty17/mirror-leech-telegram-bot"
 try:
     if len(UPSTREAM_BRANCH) == 0:
        raise TypeError
